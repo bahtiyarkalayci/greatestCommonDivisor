e9413de89ae5365cb35070a0f7b5695a25efc6ac
bahtiyarkalayci <49812375+bahtiyarkalayci@users.noreply.github.com>
2022-11-15 09:48:12 +0300

    https://www.patika.dev/tr

--- src/Gcd.java
+++ src/Gcd.java
@@ -1,2 +1,33 @@
-package PACKAGE_NAME;public class Gcd {
+import java.util.Scanner;
+
+public class Gcd {
+
+    public static void main(String[] args) {
+
+        int n1,n2;
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("enter the number n1");
+        n1=scanner.nextInt();
+        System.out.println("enter the number n2");
+        n2=scanner.nextInt();
+        int i=n1;
+        int ebob=1;
+
+        while (i>=1){
+            i--;
+            if (n1%i==0&&n2%i==0) {
+                ebob=i;
+                System.out.println("Greatest Common Division="+i);
+                System.out.println("Lowest Common Multiple="+(n1*n2)/ebob);
+                break;
+            }
+
+
+
+        }
+
+
+
+
+    }
 }
